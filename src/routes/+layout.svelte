d084f082a45779ab2165457e1b817db253baa0e3
KekOnTheWorld <kekontheworld@gmail.com>
2022-09-07 19:33:52 +0200

    kekontheworld

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '@fontsource/dela-gothic-one';
+
+	import '../app.css';
+</script>
+
+<slot />
---
+++ src/routes/+layout.ts
@@ -0,0 +1,3 @@
+export const ssr = false;
+export const csr = true;
+export const prerender = true;
