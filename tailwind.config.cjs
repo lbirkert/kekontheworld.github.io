ae742fe467206c8fe44a0adbe023c59f6ccb3185
KekOnTheWorld <kekontheworld@gmail.com>
2022-09-12 20:46:03 +0200

    Darkmode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: "class",
+	darkMode: ["class", ["head.dark ~ body"]],
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
 		extend: {
