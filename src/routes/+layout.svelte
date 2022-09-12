14b42e383218d68970e6c1417adbb81ce89cc041
KekOnTheWorld <kekontheworld@gmail.com>
2022-09-12 20:45:36 +0200

    Added darkmode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
-<script>
+<script lang="ts">
+	import DarkModePre from "$lib/DarkModePre.svelte";
+
 	import "@fontsource/dela-gothic-one/latin.css";
 	import "@fontsource/inter/latin.css";
-
 	import "../app.css";
 </script>
+
+<DarkModePre/>
 
 <slot />
