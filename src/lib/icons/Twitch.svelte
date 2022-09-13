b179e64fab6b528bbcb66086c653a6cfc93b415a
KekOnTheWorld <kekontheworld@gmail.com>
2022-09-13 20:18:44 +0200

    Added icons

---
+++ src/lib/icons/Twitch.svelte
@@ -0,0 +1,7 @@
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 2400 2800" xml:space="preserve">
+<g id="Layer_1-2">
+	<path class="st1" d="M500,0L0,500v1800h600v500l500-500h400l900-900V0H500z M2200,1300l-400,400h-400l-350,350v-350H600V200h1600V1300z" fill="currentColor"/>
+	<rect x="1700" y="550" class="st1" width="200" height="600" fill="currentColor"/>
+	<rect x="1150" y="550" class="st1" width="200" height="600" fill="currentColor"/>
+</g>
+</svg>
